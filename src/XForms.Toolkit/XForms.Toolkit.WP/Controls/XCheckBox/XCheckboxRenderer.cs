df072cb58d0c813f6abfa58752d0dc4cc6865f9f
Pavel Simsa <pavel@simsa.cz>
2014-06-11 00:14:24 +0200

    Checkbox + WP8 Renderer

---
+++ src/XForms.Toolkit/XForms.Toolkit.WP/Controls/XCheckBox/XCheckboxRenderer.cs
@@ -0,0 +1,58 @@
+﻿#region
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinPhone;
+using XForms.Toolkit.Controls;
+using XForms.Toolkit.WP.Controls.XCheckBox;
+
+#endregion
+
+[assembly: ExportRenderer(typeof (XCheckBox), typeof (XCheckBoxRenderer))]
+
+namespace XForms.Toolkit.WP.Controls.XCheckBox
+{
+    internal class XCheckBoxRenderer : ViewRenderer
+    {
+        /*private bool isChecked;
+        private string _text;
+*/
+        public event OnClicked Clicked;
+
+        private CheckBox cb = new CheckBox();
+
+        public XCheckBoxRenderer()
+        {
+        }
+
+        protected override void OnModelSet()
+        {
+            base.OnModelSet();
+            var y = Model;
+            cb.IsChecked = (y as XForms.Toolkit.Controls.XCheckBox).IsChecked;
+            cb.Content = (y as XForms.Toolkit.Controls.XCheckBox).Text;
+            cb.Checked += cb_CheckChanged;
+            cb.Unchecked += cb_CheckChanged;
+
+            SetNativeControl(cb);
+        }
+
+        private void cb_CheckChanged(object sender, RoutedEventArgs args
+            )
+        {
+            var x = Model as XForms.Toolkit.Controls.XCheckBox;
+            var y = sender as CheckBox;
+            //(Model as XCheckBox).IsChecked = y.IsChecked.Value;
+            var e = new MyCbEventArgs();
+            e.Checked = y.IsChecked.Value;
+            e.Text = y.Content.ToString();
+            e.Context = y.DataContext;
+            (Model as XForms.Toolkit.Controls.XCheckBox).OnClicked(this, e);
+        }
+    }
+
+    public delegate void OnClicked(object sender, EventArgs args);
+}
---
+++ src/XForms.Toolkit/XForms.Toolkit/Controls/XCheckBox.cs
@@ -0,0 +1,53 @@
+﻿#region
+
+using System;
+using Xamarin.Forms;
+
+#endregion
+
+namespace XForms.Toolkit.Controls
+{
+    public class XCheckBox : View
+    {
+        public bool IsChecked
+        {
+            get { return (bool) GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        public string Text
+        {
+            set { SetValue(TextProperty, value); }
+            get { return (string) GetValue(TextProperty); }
+        }
+
+        public event OnClicked Clicked;
+
+        public XCheckBox()
+        {
+        }
+
+        public void OnClicked(object sender, MyCbEventArgs e)
+        {
+            IsChecked = e.Checked;
+            Text = e.Text;
+            Clicked(sender, e);
+        }
+
+        public static BindableProperty TextProperty = BindableProperty.Create("TextProperty", typeof (string),
+            typeof (string), string.Empty,
+            BindingMode.TwoWay);
+
+        public static BindableProperty IsCheckedProperty = BindableProperty.Create("IsCheckedProperty", typeof (bool),
+            typeof (bool), false, BindingMode.TwoWay);
+    }
+
+    public delegate void OnClicked(object sender, MyCbEventArgs args);
+
+    public class MyCbEventArgs : EventArgs
+    {
+        public bool Checked;
+        public string Text;
+        public object Context;
+    }
+}
