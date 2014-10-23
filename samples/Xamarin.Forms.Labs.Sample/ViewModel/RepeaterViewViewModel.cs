bf2639eb07f0659c9caddd0d20c6e4b6ca9f02ac
bluechiperic <eric.grover@bluechip-llc.com>
2014-10-23 10:06:57 -0400

    Added Index to Thing Model

--- samples/Xamarin.Forms.Labs.Sample/ViewModel/RepeaterViewViewModel.cs
+++ samples/Xamarin.Forms.Labs.Sample/ViewModel/RepeaterViewViewModel.cs
@@ -81,6 +81,7 @@ namespace Xamarin.Forms.Labs.Sample.ViewModel
 
     public class Thing
     {
+        public int Index { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
