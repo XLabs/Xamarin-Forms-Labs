3f81674d4a188926781d374bfb4acbaaa025f63e
cliff vaughn <powerdude@aol.com>
2016-06-11 16:47:00 -0400

    added XFormsApp classes for FormsAppCompatActivity

--- source/Forms/XLabs.Forms.Droid/XFormsApplicationCompatDroid.cs
+++ source/Forms/XLabs.Forms.Droid/XFormsApplicationCompatDroid.cs
@@ -6,7 +6,7 @@
 // Last Modified By : XLabs Team
 // Last Modified On : 01-04-2016
 // ***********************************************************************
-// <copyright file="XFormsApp.cs" company="XLabs Team">
+// <copyright file="XFormsApplicationCompatDroid.cs" company="XLabs Team">
 //     Copyright (c) XLabs Team. All rights reserved.
 // </copyright>
 // <summary>
@@ -34,9 +34,9 @@ using Environment = Android.OS.Environment;
 namespace XLabs.Forms
 {
     /// <summary>
-    /// Class XFormsApplicationDroid.
+    /// Class XFormsApplicationCompatDroid.
     /// </summary>
-    public class XFormsApplicationDroid : FormsApplicationActivity
+    public class XFormsApplicationCompatDroid : FormsAppCompatActivity
     {
         /// <summary>
         /// Gets or sets the destroy.
@@ -295,58 +295,6 @@ namespace XLabs.Forms
             }
 
             base.OnStop();
-        }
-    }
-
-    /// <summary>
-    /// Class XFormsAppDroid.
-    /// </summary>
-    public class XFormsAppDroid : XFormsApp<XFormsApplicationDroid>
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="XFormsAppDroid"/> class.
-        /// </summary>
-        public XFormsAppDroid() { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="XFormsAppDroid"/> class.
-        /// </summary>
-        /// <param name="app">The application.</param>
-        public XFormsAppDroid(XFormsApplicationDroid app) : base(app) { }
-
-        /// <summary>
-        /// Raises the back press.
-        /// </summary>
-        public void RaiseBackPress()
-        {
-            this.OnBackPress();
-        }
-
-        /// <summary>
-        /// Called when [initialize].
-        /// </summary>
-        /// <param name="app">The application.</param>
-        /// <param name="initServices">Should initialize services.</param>
-        protected override void OnInit(XFormsApplicationDroid app, bool initServices = true)
-        {
-            this.AppContext.Start += (o, e) => this.OnStartup();
-            this.AppContext.Stop += (o, e) => this.OnClosing();
-            this.AppContext.Pause += (o, e) => this.OnSuspended();
-            this.AppContext.Resume += (o, e) => this.OnResumed();
-            this.AppDataDirectory = Environment.ExternalStorageDirectory.AbsolutePath;
-
-            if (initServices) 
-            {
-                DependencyService.Register<TextToSpeechService> ();
-                DependencyService.Register<Geolocator> ();
-                DependencyService.Register<MediaPicker> ();
-                DependencyService.Register<SoundService> ();
-                DependencyService.Register<EmailService> ();
-                DependencyService.Register<FileManager> ();
-                DependencyService.Register<AndroidDevice> ();
-            }
-
-            base.OnInit(app);
         }
     }
 }
